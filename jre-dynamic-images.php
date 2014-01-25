312bc09ad461d943077c70736f91e8ba8738a87f
Dan <ross9885@gail.com>
2014-01-24 23:57:14 -0800

    Initial commit.

---
+++ jre-dynamic-images.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Jo Real Estate: Dynamic Images
+Version: 0.1-alpha
+Description: Populates image and gallery placeholders with images, matched by section name and size.
+Author: Dan Ross <ross9885@gmail.com>
+*/
+
