c00b96a61a51c243a3d73de5cb5a4793eec68d35
Dan <ross9885@gail.com>
2014-01-30 19:40:37 -0800

    Rotate an active class across a sequence of images, instead of actually rotating image elements. Avoid specifying carousel dimensions be making the first element position: relative.

--- scripts/dynamic-images-carousel.js
+++ scripts/dynamic-images-carousel.js
@@ -6,39 +6,31 @@ jQuery( document ).ready( function( $ ) {
 
 	function init( ctx ) {
 		$( $( ctx ).children()[0] ).addClass( 'active' ); // The first child of this carousel is active.
-		$( ctx ).addClass( 'active' ); // This carousel is active.
+		var period = $( ctx ).attr( 'data-period' ) || 3000;
 
 		setTimeout( function() {
-			rotate( ctx );
-		}, 8000 );
+			rotate( ctx, period );
+		}, period );
 	}
 
-	function rotate( ctx ) {
+	function rotate( ctx, period ) {
 		console.log( 'rotating' );
-		var bottom = $( $( ctx ).children().last() ).remove();
-		$( ctx ).prepend( bottom );
+		var current = $( ctx ).children( '.active' );
+		var next = current.next();
+
+		if( next.length === 0 ) {
+			next = $( ctx ).children( ':first-child' );
+		}
+
+		current.toggleClass( 'active' );
+		next.toggleClass( 'active' );
 
 		setTimeout( function() {
-			rotate( ctx );
-		}, 8000 );
+			rotate( ctx, period );
+		}, period );
 	}
 
 	$( '.dynamicImagesCarousel' ).each( function() {
-
-		var that = this;
-		function resizeCarousel() {
-			// Set the height of the carousel to match it's contents.
-
-			var height = $( that ).children( ':first' ).height();
-			$( that ).height( height );
-			console.log( height );
-		}
-
-		$( window ).resize( resizeCarousel ); // Resize the carousel whenever th browser window is resized.
-		$( this ).children( ':first' ).load( resizeCarousel ); // Resize the carousel as soon as we know how tall the images are.
-		resizeCarousel(); // Resize imediately in case the images have loaded before this function executes.
-
 		init( this ); // Set up the carousel and start rotating images.
 	});
-
 });
